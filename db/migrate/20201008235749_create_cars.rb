4da4c14a576e3aeec2d9e3eaa5d9278e0d5ba644
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 18:58:09 -0500

    added car model

---
+++ db/migrate/20201008235749_create_cars.rb
@@ -0,0 +1,15 @@
+class CreateCars < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :color
+      t.string :year
+      t.text :description
+      t.string :miles
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
