3f8d5b0ed460e78bff20f9e7eadd1ec4e03e5bd8
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 21:21:01 -0500

    added index and show methods in car controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
     #Cars
     get "/cars" => "cars#index"
-    get "/cars/id" => "cars#show"
+    get "/cars/:id" => "cars#show"
     post "/cars" => "cars#create"
     patch "cars/:id" => "cars#update"
     delete "cars/:id" => "cars#destroy"
