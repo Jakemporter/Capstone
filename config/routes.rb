1607058ac7d1552ede52dbe903f18694831c0bbb
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 20:55:44 -0500

    added some routes and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
 
   namespace :api do
-
-
+    #Users
     post "/users" => "users#create"
+    #Login
+    post "/sessions" => "sessions#create"
+    #Cars
+    
+
   end
 end
