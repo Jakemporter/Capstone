be968c3094d3227037a1082e9a2df3dc10cf05e5
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 19:50:17 -0500

    added accociations between models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,8 @@
 class Car < ApplicationRecord
+  has_many :bids
+  belongs_to :user
+  has_many :images
+  has_many :comments
+  has_many :category_cars
+  has_many :categories, through: :category_cars
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+
+  has_many :cars
+  has_many :bids
+  has_many :comments
 end
