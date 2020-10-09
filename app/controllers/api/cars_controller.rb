bd4859036fcaf6bf598b799b31a662a8c24d21cb
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 22:04:15 -0500

    added create method for cars

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -1,5 +1,5 @@
 class Api::CarsController < ApplicationController
-
+  before_action :authenticate_user, except: [:index, :show]
   def index
     @cars = Car.all
     render "index.json.jb"
@@ -9,4 +9,27 @@ class Api::CarsController < ApplicationController
     @car = Car.find_by(id: params[:id])
     render "show.json.jb"
   end
+  
+  def create
+    @car = Car.new(
+      user_id: current_user.id,
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      year: params[:year],
+      description: params[:description],
+      miles: params[:miles],
+    )
+
+    if @car.save
+      CategoryCar.create!(category_id: params[:category_id],
+      car_id: @car.id)
+      Image.create!(car_id: @car.id, url: params[:image_url])
+    else
+      render json: {errors: @car.errors.full_messages}, status: 422
+    end
+    render "show.json.jb"
+  end
+
+  
 end
