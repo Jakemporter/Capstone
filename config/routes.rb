ee7380476d6a5b86e4ec48183e6451efcb071990
Jake Porter <Jake.m.porter@gmail.com>
2020-10-08 21:05:55 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
     #Login
     post "/sessions" => "sessions#create"
     #Cars
+    get "/cars" => "cars#index"
+    get "/cars/id" => "cars#show"
+    post "/cars" => "cars#create"
+    patch "cars/:id" => "cars#update"
+    delete "cars/:id" => "cars#destroy"
+    #Bids
+    post "/bids/:id" => "bids#show"
     
-
   end
 end
